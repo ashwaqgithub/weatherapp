c167c48ddbb76b4e600354d984b8d7ec141fb207
shoq <shoq.dh@gmail.com>
2020-05-04 05:34:21 +0300

    Initial Commit

---
+++ weatherapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherapp
+//
+//  Created by Eng.shoqi aldh on 5/4/20.
+//  Copyright © 2020 Eng.shoqi aldh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
